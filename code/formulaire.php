95d48934780f64f3286371d5639194f5fd1dd373
gabonetti16 <gaby.bonetti1609@gmail.com>
2021-09-01 11:38:45 +0200

    StructureFormulaire

--- code/formulaire.php
+++ code/formulaire.php
@@ -2,12 +2,39 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <title>Journer Sportive</title>
 </head>
+
 <body>
-    <h1>ooooy</h1>
-    
+    <h1>Inscription à la Journée Sportive du CFPT</h1>
+    <form action="#" method="POST">
+        <p>Nom:<input type="text" placeholder="Yorke">
+        </p>
+
+        <p>Prenom:<input type="text" placeholder="Tham">
+        </p>
+
+        <p>Classe:<input type="text" placeholder="IFA-P2B">
+        </p>
+
+
+        <p>Premier choix:<input type="text" placeholder="Accrobranche">
+        </p>
+
+        <p>Deuxieme choix:<input type="text" placeholder="Vélo">
+        </p>
+
+        <p>Troisième choix:<input type="text" placeholder="Football">
+        </p>
+        <input type="submit" value="Confirmer">
+        <input type="submit" value="Annuler">
+    </form>
+
+
 </body>
+<footer>
+    <p>Bonetti Gabriel</p>
+</footer>
+
 </html>
